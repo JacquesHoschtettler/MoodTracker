39163db008c4b830b5f2b4d5b278b466189b9b58
Jacques Hoschtettler <jacques.hoschtettler@gmx.com>
2018-03-18 21:58:15 +0100

    Shift the activity_week_history.xml with a relative layout. Using strings.xml resource for parametring width and padding.

--- app/src/main/java/com/hoschtettler/jacques/moodtracker/Controller/WeekHistory.java
+++ app/src/main/java/com/hoschtettler/jacques/moodtracker/Controller/WeekHistory.java
@@ -19,7 +19,6 @@ public class WeekHistory extends AppCompatActivity implements View.OnClickListen
     {
     private View[] mDaysMood = new  View[7] ;
     private ImageButton[] mDaysComment = new ImageButton[7];
-    private View[] mDaysComplement = new View[7];
     private ArrayList<Mood> mWeeklyMoods ;
 
     private MoodList mReferencedMoods;     // List of the referenced moods
@@ -39,25 +38,18 @@ public class WeekHistory extends AppCompatActivity implements View.OnClickListen
         // Plugging the elements of the history display
         mDaysMood[0] = (View) findViewById(R.id.sevenDaysAgo_view) ;
         mDaysComment[0] = (ImageButton) findViewById(R.id.sevenDaysComment_btn) ;
-        mDaysComplement[0]= (View) findViewById(R.id.sevenDaysAgoComplement_view) ;
         mDaysMood[1] = (View) findViewById(R.id.sixDaysAgo_view) ;
         mDaysComment[1] = (ImageButton) findViewById(R.id.sixDaysComment_btn) ;
-        mDaysComplement[1]= (View) findViewById(R.id.sixDaysAgoComplement_view) ;
         mDaysMood[2] = (View) findViewById(R.id.fiveDaysAgo_view) ;
         mDaysComment[2] = (ImageButton) findViewById(R.id.fiveDaysComment_btn) ;
-        mDaysComplement[2]= (View) findViewById(R.id.fiveDaysAgoComplement_view) ;
         mDaysMood[3] = (View) findViewById(R.id.fourDaysAgo_view) ;
         mDaysComment[3] = (ImageButton) findViewById(R.id.fourDaysComment_btn) ;
-        mDaysComplement[3]= (View) findViewById(R.id.fourDaysAgoComplement_view) ;
         mDaysMood[4] = (View) findViewById(R.id.threeDaysAgo_view) ;
         mDaysComment[4] = (ImageButton) findViewById(R.id.threeDaysComment_btn) ;
-        mDaysComplement[4]= (View) findViewById(R.id.threeDaysAgoComplement_view) ;
         mDaysMood[5] = (View) findViewById(R.id.twoDaysAgo_view) ;
         mDaysComment[5] = (ImageButton) findViewById(R.id.twoDaysComment_btn) ;
-        mDaysComplement[5]= (View) findViewById(R.id.twoDaysAgoComplement_view) ;
         mDaysMood[6] = (View) findViewById(R.id.yesterday_view) ;
         mDaysComment[6] = (ImageButton) findViewById(R.id.yesterdayComment_btn) ;
-        mDaysComplement[6]= (View) findViewById(R.id.yesterdayComplement_view) ;
 
         // Identifying the buttons
         for (int i = 0 ; i <7 ; ++i)
